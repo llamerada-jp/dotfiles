e01556ac7935caa1fba97c8c0397c58cd3cd29f7
Yuji Ito <llamerada.jp@gmail.com>
2016-05-09 16:54:12 +0900

    add php-mode

--- emacs/init.el
+++ emacs/init.el
@@ -175,6 +175,20 @@
     '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
 
 
+;; php-mode
+(require 'php-mode)
+(add-hook 'php-mode-hook (lambda ()
+			   (defun ywb-php-lineup-arglist-intro (langelem)
+			     (save-excursion
+			       (goto-char (cdr langelem))
+			       (vector (+ (current-column) c-basic-offset))))
+			   (defun ywb-php-lineup-arglist-close (langelem)
+			     (save-excursion
+			       (goto-char (cdr langelem))
+			       (vector (current-column))))
+			   (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+			   (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
+
 ;; web-mode
 (require 'web-mode)
 
